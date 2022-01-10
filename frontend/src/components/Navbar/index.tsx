89761a0a88a0df4d9699cb11415dccc6de472efe
Lucio Pintanel <lm.pintanel@gmail.com>
2022-01-10 14:59:25 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import './style.css';
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg'
+
+function Navbar() {
+    return (
+      <header>
+        <nav className='container'>
+          <div className='dsmovie-nav-content'>
+            <h1>DSMovie</h1>
+            <a href="https://github.com/lucioPintanel">
+              <div className='dsmovie-contact-container'>
+                <GithubIcon />
+                <p className='dsmovie-contact-link'>
+                  /LucioPintanel
+                </p>
+              </div>
+            </a>
+          </div>
+        </nav>
+      </header>
+      );
+  }
+  
+  export default Navbar;
